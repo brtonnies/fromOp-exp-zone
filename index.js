a7cd45aeb8bdcd99a1380e131b6e0341b9d86aa3
Brian Tonnies <brtonnies@gmail.com>
2014-12-18 14:17:54 -0600

    chat still working, but user notifications are not
    yup

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var usr = 0;
+var usrId = 0;
 
 // Call up index.html
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile('index.html', { root: __dirname });
 });
-
 // Console logs User Connects & Disconnects
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  usr++;
+  usrId++;
+    console.log('user(' + usrId + ') connected    |     :users: ' + usr);
+    socket.on('connection', function(usr){
+      io.emit('user connected', usr);
+    });
+    socket.on('disconnect', function(){ 
+      usr--;
+      console.log('user(' + usrId + ') disconnected |     :users: ' + usr);
+    });
   });
-});
 
 // Console logs message & number
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log('                     |   :message: ' + msg);
   });
 });
 
@@ -31,5 +38,5 @@ io.on('connection', function(socket){
 
 //console log init
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening            | :localhost: 3000');
 });
